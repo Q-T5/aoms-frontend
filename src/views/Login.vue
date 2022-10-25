81f2f30a136626542476cf797a3f2204c3008aef
Q-T5 <bikathimartin@outlook.com>
2022-10-25 17:12:20 +0300

    Did some visual UI tweaks

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="flex">
-    <div class="w-1/3 relative">
-      <img src="../assets/cat_image.jpg" alt="cat-image" class="h-screen w-fit object-cover" />
-      <p class="quote">
-      "One small pet changes coming home to an empty house to coming home."<br />
-      <span class="self-end text-lg mr-1">~ Original by: Pam Brown</span>
-      </p>
-    </div>
-    <div class="grid h-screen place-items-center w-2/3">
-      <div class="w-[40rem] shadow-md p-2 rounded">
-        <h1 class="text-3xl font-roboto tracking-wide text-center">STAFF LOGIN</h1>
-        <form class="flex flex-col space-y-3">
-          <div class="inline-flex flex-col space-y-1">
-            <label for="username-box" class="form-labels">Staff ID</label>
-            <input type="text" id="username-box" placeholder="enter staff id" 
-            class="form-input-boxes" v-model="username" ref="usernameRef" />
-          </div>
-          <div class="inline-flex flex-col space-y-1">
-            <label for="password-box" class="form-labels">Password</label>
-            <input type="password" id="password-box" class="form-input-boxes" 
-            placeholder="enter password" v-model="password" />
-          </div>
-          <div>
-            <router-link :to="{ 'name': 'GeneralView' }" type="submit" class="login-button" 
-            @click="$store.commit('displayNotification', ['successfully logged in', 
-            'daisyui-alert-success', 'success.svg'])">Login</router-link>
-            <router-link :to="{ 'name': 'Signup' }" class="signup-button">Sign Up</router-link>
-          </div>
-        </form>
+  <div class="flex justify-center" style="background-image: url('/src/assets/login_wallpaper.jpg');">
+    <div class="grid h-screen place-items-center">
+      <div class="w-[50rem] shadow-xl p-2 rounded flex">
+        <div>
+          <img src="../assets/cat_image.jpg" alt="cat-image" class="h-96 w-96 rounded-md" />
+        </div>
+        <div class="ml-2">
+          <h1 class="text-3xl font-roboto tracking-wide text-center text-white">STAFF LOGIN</h1>
+          <form class="flex flex-col space-y-3">
+            <div class="inline-flex flex-col space-y-1">
+              <p class="quote">
+                "Animals are such agreeable friends—they ask no questions; they pass no criticisms."<br />
+                <span class="self-end text-lg mr-1">~ By: George Eliot</span>
+              </p>
+              <label for="username-box" class="form-labels">Staff ID</label>
+              <input type="text" id="username-box" placeholder="enter staff id" 
+              class="form-input-boxes text-white text-xl" v-model="username" ref="usernameRef" />
+            </div>
+            <div class="inline-flex flex-col space-y-1 relative">
+              <label for="password-box" class="form-labels">Password</label>
+              <input :type="inputType" id="password-box" class="form-input-boxes text-white text-xl" 
+              placeholder="enter password" v-model="password" />
+              <font-awesome-icon icon="fa-solid fa-eye" class="absolute bottom-[15px] right-2" @click="inputType == 'password' ? inputType = 'text' : inputType = 'password' " />
+            </div>
+            <div class="w-full flex justify-center">
+              <router-link :to="{ 'name': 'GeneralView' }" type="submit" class="login-button" 
+              @click="$store.commit('displayNotification', ['successfully logged in', 
+              'daisyui-alert-success', 'success.svg'])">Login</router-link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
   <footer class="w-full flex fixed bottom-0.5 font-nunito font-semibold justify-center text-lg">
-    <p>Made with <span class="text-red-500">&hearts;</span> in 
-      <font-awesome-icon icon="fa-brands fa-java" /> and 
-      <font-awesome-icon icon="fa-brands fa-vuejs" />
+    <p class="inline-flex text-white items-center">Made with <span class="text-red-500 mx-1">&hearts;</span> in 
+      <img src="/src/assets/java-logo.svg" alt="Java Logo" class="w-7 mx-1"> and
+      <img src="/src/assets/vue.svg" alt="Vue Logo" class="w-6 mx-1">
     </p>
   </footer>
 </template>
@@ -45,7 +47,8 @@ export default {
   data() {
     return {
       "username": "",
-      "password": ""
+      "password": "",
+      "inputType": "password"
     }
   },
   mounted() {
