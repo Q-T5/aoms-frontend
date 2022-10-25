a80b103acc63f4966e3794240359d5fee9836b65
Q-T5 <bikathimartin@outlook.com>
2022-10-25 15:56:20 +0300

    Delete unused route

--- src/router.js
+++ src/router.js
@@ -3,11 +3,6 @@ import Login from "/src/views/Login.vue"
 import Dashboard from "/src/views/Dashboard.vue"
 import NotFound from "/src/views/NotFound.vue"
 import Management from "/src/views/Management.vue"
-import AdminPanel from "/src/views/AdminPanel.vue"
-import ManageUsers from "/src/views/ManageUsers.vue"
-import Notes from "/src/views/Notes.vue"
-import ManagerPanel from "/src/views/ManagerPanel.vue"
-import Statistics from "/src/views/Statistics.vue"
 
 const router = createRouter({
     "history": createWebHistory(),
@@ -49,51 +44,7 @@ const router = createRouter({
                     "component": () => import("/src/views/UserSettings.vue"),
                     "name": "UserSettings"
                 },
-                {
-                    "path": "/dashboard/admin",
-                    "component": AdminPanel,
-                    "name": "AdminPanel",
-                    "children": [
-                        {
-                            "path": "/dashboard/admin/manage-users",
-                            "component": ManageUsers,
-                            "name": "ManageUsers"
-                        },
-                        {
-                            "path": "/dashboard/admin/notes",
-                            "component": Notes,
-                            "name": "Notes"
-                        }
-                    ]
-                },
-                {
-                    "path": "/dashboard/manager",
-                    "component": ManagerPanel,
-                    "name": "ManagerPanel",
-                    "children": [
-                        {
-                            "path": "/dashboard/manager/manage-users",
-                            "component": ManageUsers,
-                            "name": "ManageUsers"
-                        },
-                        {
-                            "path": "/dashboard/manager/notes",
-                            "component": Notes,
-                            "name": "Notes"
-                        },
-                        {
-                            "path": "/dashboard/manager/statistics",
-                            "component": Statistics,
-                            "name": "Statistics"
-                        }
-                    ]
-                }
             ]
-        },
-        {
-            "path": "/signup",
-            "component": () => import("/src/views/Signup.vue"),
-            "name": "Signup"
         },
         {
             "path": "/:catchAll(.*)",
