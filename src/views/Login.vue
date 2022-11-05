a6d0a9f92315fe5945a3a3bbd556b2da2f5019d4
Q-T5 <bikathimartin@outlook.com>
2022-11-05 22:32:05 +0300

    Changed visual style of login form and background

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="flex justify-center" style="background-image: url('/src/assets/login_wallpaper.jpg');">
-    <div class="grid h-screen place-items-center">
-      <div class="w-[50rem] shadow-xl rounded flex">
-        <div>
-          <img src="../assets/cat_image.jpg" alt="cat-image" class="h-96 w-96" />
-        </div>
+  <div style="background-image: url('/src/assets/login_wallpaper.jpg');">
+    <div class="grid h-screen place-items-center backdrop-blur-sm">
+      <div class="w-[40rem] shadow-xl rounded flex">
         <div class="p-2">
-          <h1 class="text-3xl font-roboto tracking-wide text-center text-white">STAFF LOGIN</h1>
+          <h1 class="text-3xl font-roboto tracking-wide text-center text-white">SYSTEM LOGIN</h1>
           <form class="flex flex-col space-y-3">
             <div class="inline-flex flex-col space-y-1">
               <p class="quote">
@@ -21,7 +18,8 @@
               <label for="password-box" class="form-labels">Password</label>
               <input :type="inputType" id="password-box" class="form-input-boxes text-white text-lg" 
               placeholder="enter password" v-model="password" />
-              <font-awesome-icon icon="fa-solid fa-eye" class="absolute bottom-[15px] right-2" @click="inputType == 'password' ? inputType = 'text' : inputType = 'password' " />
+              <font-awesome-icon icon="fa-solid fa-eye" class="absolute bottom-[15px] right-2" 
+              @click="inputType == 'password' ? inputType = 'text' : inputType = 'password' " />
             </div>
               <router-link :to="{ 'name': 'GeneralView' }" type="submit" class="login-button" 
               @click="$store.commit('displayNotification', ['successfully logged in', 
