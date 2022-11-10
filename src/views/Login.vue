8ad0a626bb5f6f421ec2cd391488d2758eed833d
Q-T5 <bikathimartin@outlook.com>
2022-11-10 21:03:26 +0300

    Added login logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background-image: url('/src/assets/login_wallpaper.jpg');">
+  <div style="background-image: url('/src/assets/login_wallpaper.jpg')">
     <div class="grid h-screen place-items-center backdrop-blur-sm">
       <div class="w-[40rem] shadow-xl rounded flex">
         <div class="p-2">
@@ -12,18 +12,17 @@
               </p>
               <label for="username-box" class="form-labels">Staff ID</label>
               <input type="text" id="username-box" placeholder="enter staff id" 
-              class="form-input-boxes text-white text-lg" v-model="username" ref="usernameRef" />
+              class="form-input-boxes text-white text-lg" v-model="userCredentials.staffId" ref="usernameRef" />
             </div>
             <div class="inline-flex flex-col space-y-1 relative">
               <label for="password-box" class="form-labels">Password</label>
               <input :type="inputType" id="password-box" class="form-input-boxes text-white text-lg" 
-              placeholder="enter password" v-model="password" />
+              placeholder="enter password" v-model="userCredentials.password" />
               <font-awesome-icon icon="fa-solid fa-eye" class="absolute bottom-[15px] right-2" 
               @click="inputType == 'password' ? inputType = 'text' : inputType = 'password' " />
             </div>
               <router-link :to="{ 'name': 'GeneralView' }" type="submit" class="login-button" 
-              @click="$store.commit('displayNotification', ['successfully logged in', 
-              'daisyui-alert-success', 'success.svg'])">Login</router-link>
+              @click="login()">Login</router-link>
           </form>
         </div>
       </div>
@@ -42,9 +41,36 @@ export default {
   "name": "Login",
   data() {
     return {
-      "username": "",
-      "password": "",
+      "userCredentials": {
+        "staffId": "",
+        "password": "",
+      },
       "inputType": "password"
+    }
+  },
+  "methods": {
+    login() {
+      if(this.userCredentials.staffId !== "" && this.userCredentials.password !== "") {
+        fetch("http://localhost:8080/api/auth/signin", {
+          "method": "POST",
+          "headers": {
+            "Content-Type": "application/json"
+          },
+          "body": JSON.stringify(this.userCredentials)
+        }).then((response) => {
+          if(response.status === 200) {
+            this.$store.commit("LOGIN_USER", response.json());
+            this.$store.commit("displayNotification", ["successful login", "daisyui-alert-success", "success.svg"]);
+            this.$router.push({ name: "GeneralView" });
+          } else if(response.status === 500) {
+            this.$store.commit("displayNotification", ["invalid login", "daisyui-alert-error", "error.svg"]);
+          }
+        }).catch((error) => {
+          console.log(`${ error }: Failed to log you in`);
+        })
+      } else {
+        this.$store.commit("displayNotification", ["please enter your logins", "daisyui-alert-error", "error.svg"]);
+      }
     }
   },
   mounted() {
