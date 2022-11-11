f411f686987cba6d4b12d930b472c3e62e462228
Q-T5 <bikathimartin@outlook.com>
2022-11-11 20:06:28 +0300

    Added list pagination component

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faJava, faVuejs } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { 
-    faHome, faPowerOff, faUser, faSun, faMoon, faPencil, faTrash, faAdd, faRepeat, faEye
+    faHome, faPowerOff, faUser, faSun, faMoon, faPencil, faTrash, faAdd, faRepeat, faEye,
+    faAngleDoubleLeft, faAngleDoubleRight
  } from '@fortawesome/free-solid-svg-icons'
 import store from './store'
 
@@ -15,7 +16,8 @@ const emitter = mitt();
 const app = createApp(App);
 
 library.add(
-    faMoon, faSun, faJava, faVuejs,faHome, faPowerOff, faUser, faPencil, faTrash, faAdd, faRepeat, faEye
+    faMoon, faSun, faJava, faVuejs,faHome, faPowerOff, faUser, faPencil, faTrash, faAdd, faRepeat, faEye,
+    faAngleDoubleLeft, faAngleDoubleRight
 );
 
 app.config.globalProperties.emitter = emitter;
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="flex flex-col py-2">
+  <div class="flex flex-col p-2">
     <div class="relative border-blue-500 border-[1px] rounded-full self-center outline-none mb-0.5 w-fit">
       <input type="number" placeholder="Search by animal id... " class="search-box" maxlength="25"
       v-model="searchId" />
       <button class="search-button">Search</button>
     </div>
-    <table class="w-full">
-      <tr class="bg-blue-500 font-poppins">
-        <th class="table-headers">Animal Id</th>
-        <th class="table-headers">Pet Name</th>
-        <th class="table-headers">Common Name</th>
-        <th class="table-headers">Age(Years)</th>
-        <th class="table-headers">Date Brought</th>
-        <th class="text-base py-0 text-center font-roboto font-normal text-white">Action</th>
-      </tr>
-      <tr class="table-row" v-for="(animal, index) in animalList" :key="animal.index">
-        <td> {{ animal.animalId }} </td>
-        <td> {{ animal.petName }}</td>
-        <td> {{ animal.commonName }}</td>
-        <td> {{ animal.age }}</td>
-        <td> {{ animal.dateBrought }}</td>
-        <td class="text-center">
-          <router-link :to="{ 'name': 'EditorView' }" class="text-blue-500/80 text-sm" 
-          @click="updateAnimal(index)"><font-awesome-icon icon="fa-solid fa-pencil" /></router-link> |
-          <button class="text-red-500/80 text-sm" @click="deleteAnimal(index)">
-          <font-awesome-icon icon="fa-solid fa-trash" /></button>
-        </td>
-      </tr>
-    </table>
+    <div class="flex flex-col justify-between h-full">
+      <table class="w-full">
+        <tr class="bg-blue-500 font-poppins">
+          <th class="table-headers">Animal Id</th>
+          <th class="table-headers">Pet Name</th>
+          <th class="table-headers">Common Name</th>
+          <th class="table-headers">Age(Years)</th>
+          <th class="table-headers">Date Brought</th>
+          <th class="text-base py-0 text-center font-roboto font-normal text-white">Action</th>
+        </tr>
+        <tr class="table-row" v-for="(animal, index) in animalList" :key="animal.index">
+          <td> {{ animal.animalId }} </td>
+          <td> {{ animal.petName }}</td>
+          <td> {{ animal.commonName }}</td>
+          <td> {{ animal.age }}</td>
+          <td> {{ animal.dateBrought }}</td>
+          <td class="text-center">
+            <router-link :to="{ 'name': 'EditorView' }" class="text-blue-500/80 text-sm" 
+            @click="updateAnimal(index)"><font-awesome-icon icon="fa-solid fa-pencil" /></router-link> |
+            <button class="text-red-500/80 text-sm" @click="deleteAnimal(index)">
+            <font-awesome-icon icon="fa-solid fa-trash" /></button>
+          </td>
+        </tr>
+      </table>
+      <div class="daisyui-btn-group w-full flex justify-center">
+        <button class="daisyui-btn rounded-md"><font-awesome-icon icon="fa-solid fa-angle-double-left" /></button>
+        <button class="daisyui-btn uppercase">Page 1</button>
+        <button class="daisyui-btn rounded-md"><font-awesome-icon icon="fa-solid fa-angle-double-right" /></button>
+      </div>
+    </div>
   </div>
 </template>
 
